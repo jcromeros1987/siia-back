97a52e3896cffa37fecd08e8f385ced638647316
geromt <gerardomartinez205@gmail.com>
2026-02-08 07:51:31 -0600

    feat: Add CatalogoProducto model

---
+++ cvu/models/catalogo_productos_model.py
@@ -0,0 +1,16 @@
+import uuid
+
+from django.db import models
+
+
+class CatalogoProducto(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nombre = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "cvu_catalogo_productos"
+        verbose_name = "Catalogo Producto"
+        verbose_name_plural = "Catalogo Productos"
+
+    def __str__(self):
+        return self.nombre
