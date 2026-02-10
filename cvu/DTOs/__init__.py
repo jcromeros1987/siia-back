cc9ea960e8abb471a3e33a2af481d08d6dcb4967
geromt <gerardomartinez205@gmail.com>
2026-02-10 00:48:16 -0600

    feat: Add ProductoInvestigadorDTO and checker classes for data handling

--- cvu/DTOs/__init__.py
+++ cvu/DTOs/__init__.py
@@ -1,1 +1,5 @@
 from .catalogo_producto_dto import CatalogoProductoDTO
+from .producto_investigador_dto import (
+    ProductoInvestigadorDTO,
+    ProductoInvestigadorCheckerDTO,
+)
--- cvu/DTOs/producto_investigador_dto.py
+++ cvu/DTOs/producto_investigador_dto.py
@@ -7,3 +7,15 @@ class ProductoInvestigadorCheckerDTO:
     id: UUID
     tipo: str
     investigador: UUID
+
+
+@dataclass
+class ProductoInvestigadorDTO:
+    id: UUID
+    tipo: str
+    investigador: UUID
+    contenido: dict
+    eje: str
+    titulo: str
+    status: bool
+    is_from_file: bool
