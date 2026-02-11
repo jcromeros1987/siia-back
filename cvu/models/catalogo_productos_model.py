04f24d2aa53a2a0c38b0b284e513aa63f5126125
geromt <gerardomartinez205@gmail.com>
2026-02-10 23:12:15 -0600

    feat: Add label field to CatalogoProducto and update to_dto method

--- cvu/models/catalogo_productos_model.py
+++ cvu/models/catalogo_productos_model.py
@@ -8,6 +8,7 @@ from cvu.DTOs import CatalogoProductoDTO
 class CatalogoProducto(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nombre = models.CharField(max_length=255)
+    label = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         db_table = "cvu_catalogo_productos"
@@ -18,4 +19,4 @@ class CatalogoProducto(models.Model):
         return self.nombre
 
     def to_dto(self) -> CatalogoProductoDTO:
-        return CatalogoProductoDTO(nombre=self.nombre)
+        return CatalogoProductoDTO(nombre=self.nombre, label=self.label)
