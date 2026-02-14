a11ba3fb8f158127b89d741abac5ddcc9eb74767
geromt <gerardomartinez205@gmail.com>
2026-02-14 16:01:49 -0600

    feat: Update CVU URL routing to use versioned API path

--- cvu/urls.py
+++ cvu/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from cvu.views import CVUView
 
 router = DefaultRouter()
-router.register(r"v1/cvu", CVUView, basename="cvu")
+router.register(r"", CVUView, basename="cvu")
 
 urlpatterns = router.urls
